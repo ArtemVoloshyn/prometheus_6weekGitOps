d53368a9159bcce8eb7268a0b444719a78ee27a5
ar-volos <136924495+ar-volos@users.noreply.github.com>
2023-06-27 23:15:08 +0000

    initial pipeline

---
+++ pipeline/jenkins.groovy
@@ -0,0 +1,17 @@
+pipeline {
+    agent any
+
+        choice(name: 'OS', choices: ['linux', 'darwin', 'windows', 'all'], description: 'Pick OS')
+
+    }
+    stages {
+        stage('Example') {
+            steps {
+                echo "Build for platform ${params.OS}"
+
+                echo "Build for arch: ${params.ARCH}"
+
+            }
+        }
+    }
+}
