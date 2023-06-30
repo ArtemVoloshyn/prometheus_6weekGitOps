2d36c69715a50af63c4f7f011aa8e19c70b0ace8
ar-volos <136924495+ar-volos@users.noreply.github.com>
2023-06-30 21:13:55 +0000

    corrected parameters to sensible

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -2,8 +2,8 @@ pipeline {
     agent any
     parameters {
         
-        choice(name: 'OS', choices: ['linux', 'darwin', 'windows', 'all'], description: 'Pick OS')
-        choice(name: 'ARCH', choices: ['amd64', 'arm64', 'all'], description: 'Pick ARCH')
+        choice(name: 'OS', choices: ['linux', 'darwin', 'windows'], description: 'Pick OS')
+        choice(name: 'ARCH', choices: ['amd64', 'arm64'], description: 'Pick ARCH')
     }
     stages {
         stage('Example') {
