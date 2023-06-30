e6a59f1366394ea6602bf4591f815ee017313cc6
ar-volos <136924495+ar-volos@users.noreply.github.com>
2023-06-30 20:54:12 +0000

    correct syntaxis

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -1,6 +1,7 @@
 pipeline {
-    agent any {
-
+    agent any
+    parameters {
+        
         choice(name: 'OS', choices: ['linux', 'darwin', 'windows', 'all'], description: 'Pick OS')
         choice(name: 'ARCH', choices: ['amd64', 'arm64', 'all'], description: 'Pick ARCH')
     }
