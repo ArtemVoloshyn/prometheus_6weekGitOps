fbc8986f6a7330f933061089c437c905bdf7e390
ar-volos <136924495+ar-volos@users.noreply.github.com>
2023-06-30 20:39:36 +0000

    added arch value

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -2,7 +2,7 @@ pipeline {
     agent any
 
         choice(name: 'OS', choices: ['linux', 'darwin', 'windows', 'all'], description: 'Pick OS')
-
+        choice(name: 'ARCH', choices: ['amd64', 'arm64', 'all'], description: 'Pick ARCH')
     }
     stages {
         stage('Example') {
